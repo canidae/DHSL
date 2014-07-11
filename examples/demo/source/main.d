84ad7f6fe902031a8ad640ed7465b913bc529219
Vidar Wahlberg <canidae@exent.net>
2014-07-11 22:26:26 +0200

    fixed indentation, fixed demo

--- examples/demo/source/main.d
+++ examples/demo/source/main.d
@@ -1,12 +1,13 @@
 import dhsl;
+
 import std.stdio;
 
 void main() {
-	addDynamicHandler(new DynamicFileHttpHandler("source/dhsl/httpserver.d"));
+	addDynamicHandler(new DynamicFileHttpHandler("source/main.d"));
 	writeln("spawning listener thread");
 	startServer(ServerSettings());
-	writeln("sleeping 10 seconds");
-	core.thread.Thread.sleep(dur!"seconds"(10));
+	writeln("sleeping 1000 seconds");
+	core.thread.Thread.sleep(dur!"seconds"(1000));
 	writeln("telling listener thread to shut down");
 	stopServer();
 	writeln("exiting");
