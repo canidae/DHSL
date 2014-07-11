3c71b3e5538207fc362085319ae8b72c07db0515
Vidar Wahlberg <canidae@exent.net>
2014-07-11 22:27:22 +0200

    more indentation fixing

--- examples/demo/source/main.d
+++ examples/demo/source/main.d
@@ -3,12 +3,12 @@ import dhsl;
 import std.stdio;
 
 void main() {
-	addDynamicHandler(new DynamicFileHttpHandler("source/main.d"));
-	writeln("spawning listener thread");
-	startServer(ServerSettings());
-	writeln("sleeping 1000 seconds");
-	core.thread.Thread.sleep(dur!"seconds"(1000));
-	writeln("telling listener thread to shut down");
-	stopServer();
-	writeln("exiting");
+    addDynamicHandler(new DynamicFileHttpHandler("source/main.d"));
+    writeln("spawning listener thread");
+    startServer(ServerSettings());
+    writeln("sleeping 1000 seconds");
+    core.thread.Thread.sleep(dur!"seconds"(1000));
+    writeln("telling listener thread to shut down");
+    stopServer();
+    writeln("exiting");
 }
